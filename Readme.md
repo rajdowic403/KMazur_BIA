d3d8a54f47aa137fdebbe637b47a37bbcaaf6184
admstud <admstud@MX1-Docker-09.EDU-WSB.local>
2026-03-08 18:33:42 +0100

    Działająca warstwa logiki backendu

--- Readme.md
+++ Readme.md
@@ -16,5 +16,6 @@ Ten projekt został zaplanowany z myślą o usługach PaaS (Platform as a Servic
 * [x] **Artefakt 1:** Zaplanowano strukturę folderów i diagram C4 (dostępny w `/docs`).
 * [x] **Artefakt 2:** Środowisko wielokontenerowe uruchomione lokalnie.
 * [x] **Artefakt 3:** Działająca warstwa prenzentacji.
+* [x] **Artefakt 4:** Działająca warstwa logiki backendu
 
 > **Informacja:** Ten plik będzie ewoluował. W kolejnych etapach dodamy tutaj sekcje 'Quick Start', opis zmiennych środowiskowych oraz instrukcję wdrożenia (CI/CD).
--- frontend/src/pages/Dashboards.tsx
+++ frontend/src/pages/Dashboards.tsx
@@ -1,39 +1,39 @@
 import { useEffect, useState } from 'react';
 import api from '../services/api';
 interface CloudTask {
-  id: number;
-  name: string;
+id: number;
+name: string;
   isCompleted: boolean;
 }
 const Dashboard = () => {
-  const [items, setItems] = useState<CloudTask[]>([]);
-  const [error, setError] = useState("");
+  const[items, setItems] = useState<CloudTask[]>([]);
+  const[error, setError] = useState("");
   useEffect(() => {
-    api.get('/tasks')
-      .then((res: any) => {
-        setItems(res.data);
-      })
-      .catch((err: any) => {
-        console.error("Szczegóły błędu:", err);
-        setError("Błąd połączenia z API. Sprawdź, czy kontener cloud-backend działa na porcie 8081.");
-      });
-  }, []);
-  return (
-    <div style={{ padding: '20px', textAlign: 'center', fontFamily: 'Arial, sans-serif' }}>
-      <h1>☁️ Cloud App Dashboard</h1>
-      {error && (
-        <div style={{ background: '#fff3cd', color: '#856404', padding: '10px', borderRadius: '5px', margin: '20px auto', maxWidth: '400px' }}>
-          {error}
-        </div>
-      )}
-      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
-        {items.length === 0 && !error && <p>Brak zadań w bazie. Dodaj coś przez Swaggera ({import.meta.env.VITE_API_URL})!</p>}
-        <ul style={{ listStyle: 'none', padding: 0 }}>
-          {items.map((item) => (
-            <li key={item.id} style={{ 
-              background: '#f8f9fa', 
-              margin: '5px', 
-              padding: '10px 20px', 
+api.get('/tasks')
+.then((res: any) => {
+setItems(res.data);
+})
+.catch((err: any) => {
+console.error("Szczegóły błędu:", err);
+setError("Błąd połączenia z API. Sprawdź, czy kontenercloud-backend działa na porcie8081.");
+});
+}, []);
+return (
+<div style={{ padding: '20px', textAlign: 'center', fontFamily: 'Arial, sans-serif' }}>
+<h1>☁️ Cloud App Dashboard</h1>
+{error && (
+<div style={{ background: '#fff3cd', color: '#856404', padding: '10px', borderRadius: '5px', margin: '20px auto', maxWidth: '400px' }}>
+{error}
+</div>
+)}
+<div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+{items.length === 0 && !error && <p>Brak zadań w bazie. Dodaj coś przez Swaggera ({import.meta.env.VITE_API_URL})!</p>}
+<ul style={{ listStyle: 'none', padding: 0 }}>
+{items.map((item) => (
+            <li key={item.id} style={{
+              background: '#f8f9fa',
+              margin: '5px',
+              padding: '10px 20px',
               borderRadius: '8px',
               borderLeft: item.isCompleted ? '5px solid green' : '5px solid gray',
               width: '350px',
@@ -41,11 +41,11 @@ const Dashboard = () => {
             }}>
               <strong>{item.name}</strong> {item.isCompleted ? '✅' : '⏳'}
             </li>
-          ))}
-        </ul>
-      </div>
-    </div>
-);
-};
+))}
+</ul>
+</div>
+</div>
+  );
+}
 
 export default Dashboard;
